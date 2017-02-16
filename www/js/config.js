7ac61eeb9b7e90383467023455664bf2caa721d3
altinukshini <altin.ukshini@gmail.com>
2017-02-16 18:46:29 +0100

    ios7 style fixes, changed to vers 1.0.0, https in FMS_URL

--- www/js/config.js
+++ www/js/config.js
@@ -7,7 +7,7 @@ var CONFIG = {
 
     // URL of the fixmystreet install to report to. See also config.xml-example
     // Make sure it does *not* end with a slash.
-    FMS_URL: 'http://sq.lokalizo.org',
+    FMS_URL: 'https://sq.lokalizo.org',
 
     // Name of the cobrand to use for templates, stylesheets etc.
     // Cobrand files should be placed in a new directory within www/cobrands/
